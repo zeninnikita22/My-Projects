dd86e265894b1444ed8bbb93b79d86fbe9948ebb
zeninnikita22 <zeninnikita@gmail.com>
2022-04-16 22:17:08 +0300

    minor changes

--- Simple Weather App/script.js
+++ Simple Weather App/script.js
@@ -1,5 +1,5 @@
 // const url =
-//   "https://api.openweathermap.org/data/2.5?lat=49.839683&lon=24.029717&appid=4095bb9d0a0f70a3435db12ecf8d5d1c";
+//   "url"
 // fetch(url)
 //   .then((response) => {
 //     return response.json();
