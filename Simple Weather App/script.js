8a7770979d2741bf3bff4510bb5fee27332506d0
zeninnikita22 <zeninnikita@gmail.com>
2022-04-16 22:05:38 +0300

    added html/css part of my new project

---
+++ Simple Weather App/script.js
@@ -0,0 +1,9 @@
+// const url =
+//   "https://api.openweathermap.org/data/2.5?lat=49.839683&lon=24.029717&appid=4095bb9d0a0f70a3435db12ecf8d5d1c";
+// fetch(url)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
