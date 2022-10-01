73c3d9a6bad8f0a275d79a9d283dd275a8f16b7c
Nikita <zeninnikita96@gmail.com>
2022-10-01 18:44:55 +0200

    finished collapse, basic style

--- Dead Russian Generals/dead-russian-generals/src/App.js
+++ Dead Russian Generals/dead-russian-generals/src/App.js
@@ -1,29 +1,37 @@
 import "./App.css";
 import Generals from "./Generals";
 import { useState } from "react";
-import Title from "./Title";
+import Intro from "./Intro";
 import SmoothCollapse from "react-smooth-collapse";
-import BeheadedGeneralsImage from "./Images/Beheaded.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+// import BeheadedGeneralsImage from "./Images/Beheaded.png";
 
 function App() {
   const [toggle, setToggle] = useState(false);
 
   return (
     <div className="App">
-      <div className="container">
-        <h1>
-          9 Russian Generals Killed in Ukraine{" "}
-          {toggle ? (
-            <button onClick={() => setToggle(!toggle)}>\\\</button>
-          ) : null}
-        </h1>
-        <SmoothCollapse expanded={!toggle}>
-          <Title toggle={toggle} setToggle={setToggle} />
-        </SmoothCollapse>
-        <SmoothCollapse expanded={toggle}>
-          <Generals />
-        </SmoothCollapse>
-        {/* {toggle ? <Generals /> : null} */}
+      <div className="wrapper">
+        <div className="app-box">
+          <div className="title-box">
+            <h1>RUSSIAN GENERALS KILLED IN UKRAINE</h1>
+            {toggle ? (
+              <button className="toggle" onClick={() => setToggle(!toggle)}>
+                <FontAwesomeIcon icon={faChevronUp} color="white" />
+              </button>
+            ) : null}
+          </div>
+          {/* <img className="beheaded" src={BeheadedGeneralsImage} alt="beheaded" /> */}
+
+          <SmoothCollapse expanded={!toggle}>
+            <Intro toggle={toggle} setToggle={setToggle} />
+          </SmoothCollapse>
+          <SmoothCollapse expanded={toggle}>
+            <Generals />
+          </SmoothCollapse>
+          {/* {toggle ? <Generals /> : null} */}
+        </div>
       </div>
     </div>
   );
