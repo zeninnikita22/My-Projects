85ae6c31348ad318a17fa64f2892ff19b8c7250e
Nikita <zeninnikita96@gmail.com>
2022-10-15 22:15:20 +0200

    solved currency issue

--- Last Minute Trip/last-minute-trip/src/Tickets.js
+++ Last Minute Trip/last-minute-trip/src/Tickets.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import airlines from "./data/airlines.json";
 import cities from "./data/cities.json";
@@ -10,6 +10,7 @@ function Tickets({ toggleTickets }) {
   const [code, setCode] = useState("");
   const [suggestionsClass, setSuggestionsClass] = useState(false);
   const [flights, setFlights] = useState([]);
+  const [currencyDisplay, setCurrencyDisplay] = useState("");
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -23,68 +24,71 @@ function Tickets({ toggleTickets }) {
   //   setCurrency(e.target.value);
   // };
 
-  // console.log(currency);
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(url);
-    setCurrency(e.target.elements.currency.value);
+    // setCurrency(e.target.elements.currency.value);
     console.log(currency);
+    setCurrencyDisplay(currency);
+    if (origin) {
+      const url = `https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=${code}&page=None&currency=${currency}&destination=-`;
+      console.log({ url });
+      const options = {
+        method: "GET",
+        headers: {
+          "X-Access-Token": "691bcb96d1b4bcce9ba48e48aab4c926",
+          "X-RapidAPI-Key":
+            "3e4e207afdmsh07cff4b4253a318p1950edjsnb2103f96cec8",
+          "X-RapidAPI-Host":
+            "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+        },
+      };
+      fetch(url, options)
+        .then((response) => response.json())
+        .then((response) => {
+          console.log({ response: response.data });
+          const array = Object.entries(response.data);
+          const finalArray = array
+            .map((element) => {
+              let newObj = Object.values(element[1]);
+              newObj.forEach((e) => {
+                e.airport = element[0];
+              });
+              return newObj;
+            })
+            .flat()
+            .map((item) => {
+              cities.map((element) => {
+                if (element.code === item.airport) {
+                  item = { ...item, airport: element.name };
+                }
+              });
+              item = {
+                ...item,
+                flight_number: item.airline + item.flight_number,
+              };
+              airlines.map((element) => {
+                if (element.code === item.airline) {
+                  item = { ...item, airline: element.name };
+                }
+              });
+              return item;
+            })
+            .sort((a, b) => {
+              return a.price - b.price;
+            })
+            .slice(0, 19);
+          setFlights(finalArray);
+          console.log({ finalArray });
+        })
+        .catch((err) => console.error(err));
+    }
   };
-  useEffect(() => {
-    const url = `https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=${code}&page=None&currency=${currency}&destination=-`;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-Access-Token": "691bcb96d1b4bcce9ba48e48aab4c926",
-        "X-RapidAPI-Key": "3e4e207afdmsh07cff4b4253a318p1950edjsnb2103f96cec8",
-        "X-RapidAPI-Host":
-          "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-      },
-    };
-    console.log(currency);
-    fetch(url, options)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response.data);
-        const array = Object.entries(response.data);
-        const finalArray = array
-          .map((element) => {
-            let newObj = Object.values(element[1]);
-            newObj.forEach((e) => {
-              e.airport = element[0];
-            });
-            return newObj;
-          })
-          .flat()
-          .map((item) => {
-            cities.map((element) => {
-              if (element.code === item.airport) {
-                item = { ...item, airport: element.name };
-              }
-            });
-            item = {
-              ...item,
-              flight_number: item.airline + item.flight_number,
-            };
-            airlines.map((element) => {
-              if (element.code === item.airline) {
-                item = { ...item, airline: element.name };
-              }
-            });
-            return item;
-          })
-          .sort((a, b) => {
-            return a.price - b.price;
-          })
-          .slice(0, 19);
-        setFlights(finalArray);
-      })
-      .then(console.log(flights))
-      .catch((err) => console.error(err));
-  }, [currency, code]);
 
-  const handleClick = (code, name) => {
+  // useEffect(() => {
+
+  // }, [origin, code, currency]);
+
+  const handleOriginChange = (code, name) => {
     setCode(code);
     console.log(code, name);
     setSuggestionsClass(true);
@@ -109,14 +113,18 @@ function Tickets({ toggleTickets }) {
       .catch((err) => console.error(err));
   }, [origin]);
 
-  console.log(flights);
-
   return (
     <div className={toggleTickets ? "tickets-box toggled" : "tickets-box"}>
       <p>Last Minute Tickets</p>
       <form onSubmit={handleSubmit} className="tickets-form">
         <label htmlFor="currency">Select currency</label>
-        <select name="currency" id="currency">
+        <select
+          onChange={(e) => {
+            setCurrency(e.target.value);
+          }}
+          name="currency"
+          id="currency"
+        >
           <option className="currency-option" value="USD">
             USD
           </option>
@@ -135,19 +143,19 @@ function Tickets({ toggleTickets }) {
           value={origin}
           onChange={handleChange}
         />
-        {suggestions.map((item) => {
+        {suggestions.map(({ code, name, country_name }) => {
           return (
             <div
               className={
                 suggestionsClass ? "suggestion not-active" : "suggestion"
               }
-              key={item.code}
-              onClick={(e) => handleClick(item.code, item.name)}
+              key={code}
+              onClick={() => handleOriginChange(code, name)}
             >
               <p>
-                {item.name}, {item.country_name}
+                {name}, {country_name}
               </p>
-              <p>{item.code}</p>
+              <p>{code}</p>
             </div>
           );
         })}
@@ -156,15 +164,15 @@ function Tickets({ toggleTickets }) {
           Clear
         </button>
       </form>
-      {flights.map((item, index) => {
+      {flights.map(({ price, airport, airline, flight_number }, index) => {
         return (
           <div className="flight" key={index}>
             <p>
-              Price is {item.price} {currency}
+              Price is {price} {currencyDisplay}
             </p>
-            <p>Arrival city is {item.airport}</p>
-            <p>Airline is {item.airline}</p>
-            <p>Your flight number is {item.flight_number}</p>
+            <p>Arrival city is {airport}</p>
+            <p>Airline is {airline}</p>
+            <p>Your flight number is {flight_number}</p>
           </div>
         );
       })}
