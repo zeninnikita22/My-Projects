5b05cf1171b78346e41e5b9c6300afa427137a7d
Nikita <zeninnikita96@gmail.com>
2022-10-18 16:48:33 +0200

    working on styles

--- Last Minute Trip/last-minute-trip/src/App.js
+++ Last Minute Trip/last-minute-trip/src/App.js
@@ -1,36 +1,37 @@
 import "./App.css";
 import Tickets from "./Tickets";
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import TravelImage from "./images/travel.jpg";
 
 function App() {
   const [toggleTickets, setToggleTickets] = useState(false);
-  const [starterstate, setStarterstate] = useState(false);
-
-  useEffect(() => {
-    setStarterstate(true);
-  }, []);
 
   return (
     <div className="app">
       <div className="wrapper">
+        <nav className="navbar">
+          <img src="" alt="logo"></img>
+        </nav>
         <div className="starter-box">
-          <nav className="navbar">
-            <img src="" alt="logo"></img>
-          </nav>
-          <h1>Last Minute Trip</h1>
-          <p>
-            This website is to organize your spontaneous trip. No more long
-            planning and overthinking. Get a rest from routine, change scenery,
-            open the world with one click below.
-          </p>
-          <button
-            onClick={() => setToggleTickets(!toggleTickets)}
-            className="toggle-button"
-          >
-            Get Started!
-          </button>
-          <img src={TravelImage} alt="travel" className="travel-image" />
+          <div className="starter-text-box">
+            <h1>Start your spontaneous trip now.</h1>
+            <p>
+              This website is to organize your spontaneous trip. No more long
+              planning and overthinking. Get a rest from routine, change
+              scenery, open the world with one click below.
+            </p>
+            <div className="button-box">
+              <button
+                onClick={() => setToggleTickets(!toggleTickets)}
+                className="toggle-button"
+              >
+                Start now
+              </button>
+            </div>
+          </div>
+          <div className="starter-image-box">
+            <img src={TravelImage} alt="travel" className="travel-image" />
+          </div>
         </div>
         <Tickets toggleTickets={toggleTickets} />
       </div>
