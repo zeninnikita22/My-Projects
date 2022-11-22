6733a4dc34d81427bab5f90d1c88ed0b534aceec
Nikita <zeninnikita96@gmail.com>
2022-11-22 22:06:07 +0100

    minor changes, work on initial structure of app

--- GymBro/gymbro/src/App.js
+++ GymBro/gymbro/src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button>Log In</button>
+      <button>Register</button>
+    </div>
+  );
 }
 
 export default App;
